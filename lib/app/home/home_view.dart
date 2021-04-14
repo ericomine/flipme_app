e618164bbac15204ec167abeb9abf888c7bf7802
Eric Omine <eric.omine@gmail.com>
2021-04-13 21:02:42 -0300

    refactor: cleanup summary card

--- lib/app/home/home_view.dart
+++ lib/app/home/home_view.dart
@@ -6,17 +6,13 @@ import 'package:pixel_perfect/pixel_perfect.dart';
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PixelPerfect(
-      assetPath: "assets/designs/wealth_summary.png",
-      scale: 1072.0 / 480.0,
-      child: Scaffold(
-        body: SizedBox.expand(
-          child: Container(
-            padding: const EdgeInsets.all(10.0),
-            color: context.themeData.backgroundColor,
-            child: Center(
-              child: WealthSummaryCard(),
-            ),
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          color: context.themeData.backgroundColor,
+          child: Center(
+            child: WealthSummaryCard(),
           ),
         ),
       ),
--- lib/app/home/widgets/wealth_summary_card.dart
+++ lib/app/home/widgets/wealth_summary_card.dart
@@ -2,6 +2,23 @@ import 'package:flipme_app/app/shared/extensions/context_x.dart';
 import 'package:flutter/material.dart';
 
 class WealthSummaryCard extends StatelessWidget {
+  final String total;
+  final String profitability;
+  final String cdi;
+  final String gain;
+  final void Function()? onTapThreeDots;
+  final void Function()? onTapSeeMore;
+
+  const WealthSummaryCard({
+    Key? key,
+    this.total = "R\$ 3.200.876,00",
+    this.profitability = "2,767%",
+    this.cdi = "3,45%",
+    this.gain = "R\$1833,23",
+    this.onTapThreeDots,
+    this.onTapSeeMore,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return PhysicalModel(
@@ -26,7 +43,10 @@ class WealthSummaryCard extends StatelessWidget {
               contentPadding: const EdgeInsets.all(0),
               trailing: Container(
                 width: 10,
-                child: Icon(Icons.more_vert, size: 16),
+                child: IconButton(
+                  icon: Icon(Icons.more_vert, size: 16),
+                  onPressed: onTapThreeDots,
+                ),
               ),
             ),
             SizedBox(height: 12),
@@ -34,17 +54,18 @@ class WealthSummaryCard extends StatelessWidget {
               children: [
                 Text("Valor investido", style: context.textTheme.bodyText2),
                 SizedBox(height: 5),
-                Text("R\$ 3.200.876,00",
-                    style:
-                        context.textTheme.headline6!.copyWith(fontSize: 15.5)),
+                Text(
+                  total,
+                  style: context.textTheme.headline6!.copyWith(fontSize: 15.5),
+                ),
               ],
             ),
             SizedBox(height: 21),
             Column(
               children: [
-                IndicatorRow(name: "Rentabilidade/mês", value: "2,767%"),
-                IndicatorRow(name: "CDI", value: "3,45%"),
-                IndicatorRow(name: "Ganho/mês", value: "R\$1833,23"),
+                IndicatorRow(name: "Rentabilidade/mês", value: profitability),
+                IndicatorRow(name: "CDI", value: cdi),
+                IndicatorRow(name: "Ganho/mês", value: gain),
               ],
             ),
             SizedBox(height: 8),
@@ -52,26 +73,7 @@ class WealthSummaryCard extends StatelessWidget {
             SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                GestureDetector(
-                  onTap: null,
-                  child: Container(
-                    decoration: ShapeDecoration(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        side: BorderSide(
-                          color: context.themeData.primaryColor,
-                        ),
-                      ),
-                    ),
-                    padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
-                    child: Text("VER MAIS",
-                        style: context.textTheme.bodyText2!.copyWith(
-                            color: context.themeData.primaryColor,
-                            fontWeight: FontWeight.w700)),
-                  ),
-                ),
-              ],
+              children: [SeeMoreButton(onTapSeeMore: onTapSeeMore)],
             ),
             SizedBox(height: 8),
           ],
@@ -108,6 +110,34 @@ class IndicatorRow extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SeeMoreButton extends StatelessWidget {
+  final void Function()? onTapSeeMore;
+
+  const SeeMoreButton({Key? key, required this.onTapSeeMore}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTapSeeMore,
+      child: Container(
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+            side: BorderSide(
+              color: context.themeData.primaryColor,
+            ),
+          ),
+        ),
+        padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+        child: Text("VER MAIS",
+            style: context.textTheme.bodyText2!.copyWith(
+                color: context.themeData.primaryColor,
+                fontWeight: FontWeight.w700)),
       ),
     );
   }
